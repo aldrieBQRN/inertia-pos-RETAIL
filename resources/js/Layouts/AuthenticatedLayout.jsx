10e490623b6a7c3cc794fa2dfb4bd69e6f7a6b78
Aldrie Baquiran <aldriebaquiran15@gmail.com>
2026-07-01 15:26:35 +0800

    style: increase header store name text size, weight, and mobile width constraint

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -97,7 +97,7 @@ export default function AuthenticatedLayout({ header, children, navBtn }) {
                                             <path fillRule="evenodd" d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" clipRule="evenodd" />
                                         </svg>
                                     )}
-                                    <span className="font-bold text-sm sm:text-lg text-gray-800 uppercase tracking-widest sm:tracking-[0.15em] block truncate max-w-[280px] sm:max-w-none">
+                                    <span className="font-black text-base sm:text-xl text-gray-900 uppercase tracking-wider sm:tracking-[0.15em] block truncate max-w-[340px] sm:max-w-none">
                                         {appName}
                                     </span>
                                 </Link>
